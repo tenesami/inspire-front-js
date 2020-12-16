dfc421a2a9bba88040599d4fcee8183bef3ddada
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-12-16 13:16:55 -0700

    edit the render Qutoe to use constacoter class

--- index.js
+++ index.js
@@ -1,99 +1,47 @@
-const quote_form = document.getElementById("input_form")
-
-const renderQuote = (quote, u) => {
-    const container = document.createElement('div')
-    const head = document.createElement("h2")
-    head.innerText = this.topic
-        //document.body.appendChild(head)
-    container.appendChild(head)
-
-    const qut = document.createElement('p')
-    qut.innerText = this.content
-    container.appendChild(qut)
-        //document.body.appendChild(qut)
-
-    const bloger = document.createElement('p')
-    bloger.innerText = `by: ${u.attributes.username}`
-    container.appendChild(bloger)
-
-
-    const del = document.createElement("button")
-    del.innerText = "Delete"
-    del.addEventListener("click", (e) => this.delete(e));
-    container.appendChild(del);
-    dispQuotes.appendChild(container);
-
-};
+const form_input = document.getElementById("input_form")
 
 //fetch the api 
 fetch("http://localhost:3000/quotes").
-then(function(res) {
-        return res.json()
-    })
-    .then(function(quotes) {
-        console.log(quotes)
-        quotes.data.forEach(function(quote) {
-            const u = quotes.included.find((u) =>
-                u.id == quote.relationships.user.data.id);
-            //console.log(quotes.data)
-            const quteTest = new blogQuote(quote)
-                //debugger;
-                //quteTest.display();
-                // renderQuote(quote, u)
+then(res => res.json())
 
-        });
-        blogQuote.displayAll()
+.then((quotes) => {
+    console.log(quotes)
+    quotes.data.forEach(function(quote) {
+        const u = quotes.included.find((u) =>
+            u.id == quote.relationships.user.data.id);
+        //console.log(quotes.data)
+        const newQuote = new blogQuote(quote, u)
     });
+    blogQuote.displayAll()
+});
 
 
 //Create a quote 
-const createQuote = (quotesAtt) => {
-    // return fetch("http://localhost:3000/quotes", {
-    //         method: 'POST', // or 'PUT'
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify(quotesAtt),
-    //     })
-    //     .then((res) => res.json())
-    //     .then((quotes) => {
-    //         const u = quotes.included[0];
-    //         renderQuote(quotes.data, u)
-    //     });
-};
-
 const handleForm = (e) => {
     e.preventDefault();
     //debugger
+    //if (e.target.value != "") {
     const quote = new blogQuote({
-        attributes: { topic: topic.value, content: content.value },
+        attributes: {
+            topic: topic.value,
+            content: content.value
+        },
         username: username.value,
     });
-
     quote.persist();
-
-    //debugger;
-    // createQuote({
-    //     quote: {
-    //         topic: topic.value,
-    //         content: content.value,
-    //         username: username.value
-    //     },
-    // }).then(() => {
-    //     e.target.reset();
-    // });
+    // }
 
 }
 
-quote_form.addEventListener("submit", handleForm);
+form_input.addEventListener("submit", handleForm);
 
 
 class blogQuote {
     static all = [];
+
     constructor({ id, username, attributes: { topic, content }, }) {
         this.topic = topic;
         this.content = content;
-        //this.bloger = bloger;
         this.id = id;
         this.username = username;
         this.constructor.all.push(this);
@@ -107,32 +55,23 @@ class blogQuote {
 
     display() {
         const container = document.createElement('div')
+
         const head = document.createElement("h2")
         head.innerText = this.topic
-            //document.body.appendChild(head)
         container.appendChild(head)
 
         const qut = document.createElement('p')
         qut.innerText = this.content
         container.appendChild(qut)
-            //document.body.appendChild(qut)
-
-        // const bloger = document.createElement('p')
-        // bloger.innerText = `by: ${u.attributes.username}`
-        // container.appendChild(bloger)
-
 
         const del = document.createElement("button")
         del.innerText = "Delete"
-        del.addEventListener("click", (e) => {
-            fetch(`http://localhost:3000/quotes/${this.id}`, {
-                method: "DELETE",
-            }).then(container.remove());
-        });
+        del.addEventListener("click", (e) => this.delete(e));
         container.appendChild(del);
-        document.body.appendChild(container);
 
+        dispQuotes.appendChild(container);
     }
+
     persist() {
         fetch("http://localhost:3000/quotes", {
                 method: 'POST', // or 'PUT'
@@ -153,20 +92,21 @@ class blogQuote {
                     console.log(quotes.errors)
                 } else {
                     //e.target.reset();
-                    quote_form.reset()
+                    form_input.reset()
                     this.id = quotes.data.id;
                     this.constructor.displayAll()
-                        // const u = quotes.included[0];
-                        // renderQuote(quotes.data, u)
                 }
             });
     }
 
-    delete(e) {
-        //debugger
+    delete() {
         fetch(`http://localhost:3000/quotes/${this.id}`, {
             method: "DELETE",
-        }).then(e.target.parentElement.remove());
-
+        }).then(() => {
+            this.constructor.all = this.constructor.all.filter((q) => {
+                return q != this;
+            })
+            this.constructor.displayAll();
+        });
     }
 }
